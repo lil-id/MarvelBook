fc56dc7eb073625aaf99096ad77261f4e0aef60d
Holil <71019942+lil-id@users.noreply.github.com>
2022-10-27 20:44:58 +0800

    update Auth

--- app/src/main/java/com/master/marvelbook/Auth.kt
+++ app/src/main/java/com/master/marvelbook/Auth.kt
@@ -4,8 +4,8 @@ class Constants {
 
     companion object {
         const val URL: String = "https://gateway.marvel.com/v1/public/"
-        const val APIKey: String = "c45c05a6fc201808533edd727a3a2433"
-        const val hash: String = "4f03a0b8971ed174e3c7f78d0b675709"
+        const val APIKey: String = "your_api_key"
+        const val hash: String = "the_hash"
         const val ts: String = "1"
         const val limit: Int = 50
     }
